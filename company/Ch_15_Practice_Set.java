424051131ce6b4279cc064f1ca5962a299e1407d
prashil03081999 <prashilramteke25@gmail.com>
2021-07-20 23:19:01 +0530

    String Project Java

---
+++ company/Ch_15_Practice_Set.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Ch_15_Practice_Set {
+    public static void main(String[] args) {
+
+        // Program 1:
+        // String str ="PRASHIL";
+        // System.out.println(str.toLowerCase());
+
+        // Program 2:
+        //   String str ="PRASHIL     ";
+        //   System.out.println(str.replace(' ','_'));
+
+         //  Program 3:
+        //String letter = "Dear <|name|>, Thanks a lot!";
+        //        letter = letter.replace("<|name|>", "Sachin");
+        //        System.out.println(letter);
+
+
+        // Problem 4:
+        // String myString = "This string contains double and  triple spaces";
+        // System.out.println(myString.indexOf("  "));
+        // System.out.println(myString.indexOf("   "));
+
+
+        // Problem 5
+        // String myLetter = "Dear Harry,\n\tThis Java Course is Nice.\nThanks!";
+        // System.out.println(myLetter);
+    }
+}
