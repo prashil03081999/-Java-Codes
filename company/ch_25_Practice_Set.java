02a4cceef3515f6b8344b656f8cfbeea91dd6a7f
prashil03081999 <prashilramteke25@gmail.com>
2021-07-20 23:05:57 +0530

    loop Project

---
+++ company/ch_25_Practice_Set.java
@@ -0,0 +1,94 @@
+package com.company;
+import java.util.Scanner;
+
+public class ch_25_Practice_Set {
+    public static void main(String[] args) {
+
+        // Question 1:
+        // ****
+        //***
+        //**
+        //*
+//        for(int i=4;i>=0;i--){
+//            for(int j=0;j<i;j++){
+//                System.out.print('*');
+//            }
+//
+//            System.out.print("\n");
+//        }
+//
+//
+//        // Question 2: first 5 even nos using while:
+//
+//        int i=0;
+//        int sum=0;
+//        while(i<10){
+//            System.out.println(2*i);
+//            sum=sum+2*i;
+//
+//            i++;
+//        }
+//        System.out.println("Addition of all 10 even nos are:");
+//        System.out.println(sum);
+
+
+
+
+        //Question 3: multiplication table of n
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("table of :");
+//        int n =sc.nextInt();
+//        for(int i=1;i<=10;i++){
+//            System.out.printf("%d X %d = %d",n, i, n*i);// remember that while using format string u have to use
+//            // printf I was getting error since 15 min
+//            System.out.print("\n");
+//        }
+
+
+        //Question 4: table in reverse order:
+//          Scanner sc = new Scanner(System.in);
+//          System.out.println("table of :");
+//          int n =sc.nextInt();
+//          for(int i=10;i>0;i--) {
+//              System.out.printf("%d X %d = %d", n, i, n * i);
+//              System.out.print("\n");
+//
+//          }
+
+
+
+
+        // Question 5: Factorial:
+
+//        int j=1;
+//
+//        for (int i=6;0<i;i--){
+//            j=j*i;
+//        }
+//        System.out.println(j);
+
+
+
+
+        //Question 8: addition of  table of 8
+
+//        int t=8;
+//        int mul=1;
+//        int sum=0;
+//        for(int i=1;i<=10;i++){
+//            mul=8*i;
+//            System.out.println(mul);
+//            sum=sum+mul;
+//        }
+//        System.out.println("addition of table 8 is");
+//        System.out.println(sum);
+
+
+
+
+
+
+    }
+}
