5188f0d74c52313061f112ce3a85cc58d2cef2a9
prashil03081999 <prashilramteke25@gmail.com>
2021-07-21 16:35:41 +0530

    If Else in Java

---
+++ company/Ch_16_IF_Else.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Ch_16_IF_Else {
+    public static void main(String[] args) {
+      /*  int age=20;
+        if (age<18){
+            System.out.println("U are not mature enough to drive the vehicle.");
+        }
+        else{
+            System.out.println("U are eigible to  drive the  vehicle");
+            */
+           // or  or   or   or   or   or   or
+            int age=20;
+            boolean bo=(age==20);
+            if (bo){
+                System.out.println("U are not mature enough to drive the vehicle.");
+            }
+            else{
+                System.out.println("U are eigible to  drive the  vehicle");
+
+        }
+    }
+}
