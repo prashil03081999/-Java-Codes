48c575925c6e892b8427d4e630fbe2fb2a4b87aa
prashil03081999 <prashilramteke25@gmail.com>
2021-07-21 16:32:02 +0530

    For Loop Java

---
+++ company/Ch_23_For_loop.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Ch_23_For_loop {
+    public static void main(String[] args) {
+//        for(int i=0;i<=10;i++) {
+//            System.out.println(i);
+
+            //To  get  odd no.=2i+1
+            //To  get  even no.=2i
+
+            // Print odd nos
+
+//        for(int i=0;i<=10;i++){
+//            System.out.println(2*i+1);
+//        }
+
+
+
+        // In reverse order integer
+        for(int i=5;i>0;i--){
+            System.out.println(i);
+        }
+
+    }
+}
