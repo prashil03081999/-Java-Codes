5a45d1b37efde1153e506f2a9fab5c88fb48fb87
prashil03081999 <prashilramteke25@gmail.com>
2021-07-21 16:33:32 +0530

    String methods in java

---
+++ company/ch_14_string_methods.java
@@ -0,0 +1,49 @@
+package com.company;
+import java.net.HttpURLConnection;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ch_14_string_methods {
+    public static void main(String[] args) {
+    // method 1 :calculate lengthof string.
+        //String str = "Prashil";
+        //int st=str.length(); //we have provided int cause we nned ans in int
+        //System.out.println(st);
+
+
+        // method 2&3: toLowerCase() similarly toUpperCase()
+        // likewise we do upper case and lower case just write tlc or tuc nd press enter it will write for u.
+        // String st = str.toLowerCase(); // we have provided string cause ans is in string format .
+        // System.out.println(st);
+
+        // method 4 trim : trim the spaces
+        // String NontrimString ="    ram    ";// trim will reduce the spaces.
+        // String st =NontrimString.trim();
+        // System.out.println(st);
+
+        // method 5 substring gives the value from given to last like i have given 3 it will print shil
+        //   String str = "Prashil";
+        //   System.out.println(str.substring(3));
+        //   System.out.println(str.substring(3,5)); //it will start frm index 3 to 5
+
+
+        // method 6 replace
+        // String str="harry";
+        // System.out.println(str.replace('r','p'));
+        // System.out.println(str.replace('rry','per'));// It will not run to resolve usse ""
+        // System.out.println(str.replace("rry","per"));
+        // System.out.println(str.replace("r","per")); // It will also works.
+
+
+
+        // method 7 .startsWith: it will check if starts from given char then it will print true else false
+        // Similarly .endsWith  :it will check if ends from given char then it will print true else false
+        // String st = "Prashil";
+        // System.out.println(st.endsWith("k"));
+
+
+
+
+         // method 8 and so on are given in the notes chapter 3 code with harry
+    }
+}
