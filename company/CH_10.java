c4dcc2bd7806b1133d6aa65cc8bc07329618959f
prashil03081999 <prashilramteke25@gmail.com>
2021-07-21 16:31:07 +0530

    Increment Decrement Operator Java

---
+++ company/CH_10.java
@@ -0,0 +1,15 @@
+package com.company;
+//Data Type of Expressions & Increment/Decrement Operators
+
+public class CH_10 {
+    public static void main(String[] args) {
+        int i=100;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        char ch ='a'; // if u will put a in double inverted comma u will get error
+        System.out.println(++ch);
+
+    }
+}
