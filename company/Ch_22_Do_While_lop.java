abb3eb934df27f8d65d7b70c16368e8d685d0073
prashil03081999 <prashilramteke25@gmail.com>
2021-07-22 11:49:08 +0530

    Do While Loop Practice Java

---
+++ company/Ch_22_Do_While_lop.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Ch_22_Do_While_lop {
+    public static void main(String[] args) {
+        int b=0;
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<10);  // In do while after while ; is needed otherwise u will face error.
+        // Its not needed in the simple while loop.
+
+    }
+}
