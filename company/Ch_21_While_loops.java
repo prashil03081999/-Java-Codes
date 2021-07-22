2272be88e62af9576356f7352b90931678c9f471
prashil03081999 <prashilramteke25@gmail.com>
2021-07-22 11:48:42 +0530

    While Loop Practice Java

---
+++ company/Ch_21_While_loops.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Ch_21_While_loops {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<100){
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
