afdc5f8a41884e14cdff706f33377b84ee27270a
prashil03081999 <prashilramteke25@gmail.com>
2021-07-23 11:13:24 +0530

    Retrieve Data from Array Java

---
+++ company/Ch_27_arrays.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Ch_27_arrays {
+    public static void main(String[] args) {
+//        int [] marks={45,67,43,78,53};
+//        float [] mark={45,67,43,78,53};
+
+
+
+//
+//        String [] students={"rohan","raj","nilesh","ram","sham"};
+//        System.out.println(students.length);
+//        for (int i=0;i< students.length;i++){    // to retrive the milions of data frm array use this method.
+//            System.out.println(students[i]);
+//        }
+//
+//        // or or  or or or or o
+//
+//        for (String student : students) {    // to retrive the milions of data frm array use this method
+//            System.out.println(student);     // short and simple method
+//        }
+
+
+
+
+
+//         // another way to retrieve the data
+//        int [] marks={45,67,43,78,53};
+//        for (int element: marks){      // sober way to print the data from array small sytax
+//            System.out.println(element);
+//        }
+
+        int [] age= {12,456,77,45,66,44,6,7,86,34,67,54,34,23,};
+        for(int vay :age){               // This is for each loop
+            System.out.println(vay);
+        }
+
+
+
+
+    }
+}
