73ce6b168f30b377eddc2c3192b3cf2e440d0438
prashil03081999 <prashilramteke25@gmail.com>
2021-07-23 11:12:55 +0530

    Create array Java

---
+++ company/Ch_26_Arrays.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Ch_26_Arrays {
+    public static void main(String[] args) {
+        //int [] marks=new int[5];  // first method to declaration + memory allocation
+
+//        int [] marks;
+//        marks=new int[6];       // second method to declaration and memory allocation
+
+
+//        marks[0]=96;
+//        marks[1]=90;
+//        marks[2]=85;
+//        marks[3]=56;
+//        marks[4]=45;
+//        System.out.println(marks[3]);
+
+        // Third and best way to declare, initialize,allocation  nad declare the array
+         int [] marks={45,67,43,78,53};
+        System.out.println(marks[4]);
+
+   }
+}
