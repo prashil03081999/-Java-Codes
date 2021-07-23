df874d2ada74b04190dd8333b64cb0f0ba28fc6e
prashil03081999 <prashilramteke25@gmail.com>
2021-07-23 11:11:39 +0530

    Logical Practice Java

---
+++ company/Ch_17_Logical.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Ch_17_Logical {
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b=false;
+        if (a&&b){           // similarly we can do for or gate.
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
