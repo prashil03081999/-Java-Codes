769efec549141c2d1b71e6c55b6f740624c48235
prashil03081999 <prashilramteke25@gmail.com>
2021-07-28 18:08:55 +0530

    Practice Set 1_5

---
+++ company/Ch_29_Ps_1_5.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Ch_29_Ps_1_5 {
+    public static void main(String[] args) {
+        // Question 5: Reverse the array:
+
+//        int [] arr={5,4,3,2,1};
+//        int l=arr.length;
+//        int temp;
+//        int n= Math.floorDiv(l,2); //it divide left num with right num ignoring decimals values.
+//        for(int i=0;i<n;i++) {
+//            //Swap [i] with [l-i-1]
+//            //for ex if i want to replace |3| with |4| in box i have to take 1 more | |box so i can take
+//            // 3 in that empty box i will swap 4 in 3 vala box then from temporray box i will keep 3 in 4 vala box
+//            // box1 box2 temp
+//            // |3|  |4|   | |
+//            // |4|  | |   |3|
+//            // |4|  |3|   | |
+//            // ike this we are going to swap or reverse the array
+//            temp = arr[i];
+//            arr[i] = arr[l - i - 1];
+//            arr[l - i - 1] = temp;
+//        }
+//
+//        for(int element:arr){
+//            System.out.println(element);
+//        }
+
+
+
+    }
+}
