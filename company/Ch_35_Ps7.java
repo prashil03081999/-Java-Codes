ba3cbac0a13240b60884b95a3e1239630a854f83
prashil03081999 <prashilramteke25@gmail.com>
2021-08-05 00:14:30 +0530

    Practice Set 7

---
+++ company/Ch_35_Ps7.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Ch_35_Ps7 {
+    static float temp(float n){
+        float result=0;
+        result=(n *9/5.0f) +32.0f;
+        return result;
+    }
+    public static void main(String[] args) {
+       float result=temp(23.0f);
+        System.out.println(result);
+
+
+
+    }
+}
