a8e72ee22f6ee1767bd87184ebd3bff9ba2aebc5
prashil03081999 <prashilramteke25@gmail.com>
2021-08-09 13:57:19 +0530

    Practice set 2

---
+++ company/Ch_45_Ps2.java
@@ -0,0 +1,23 @@
+package com.company;
+class cylinder1 {
+    int radius;
+    int height;
+    public cylinder1(int radius,int height){
+        this.radius=radius;
+        this.height=height;
+    }
+}
+public class Ch_45_Ps2 {
+    public static void main(String[] args) {
+        cylinder1 cyl= new cylinder1(6,9);
+        System.out.println(cyl.radius);
+        System.out.println(cyl.height);
+
+
+
+
+
+
+
+    }
+}
