32b6c6378ea56d8df157fd59062cf9ccd907c3b4
prashil03081999 <prashilramteke25@gmail.com>
2021-08-16 16:55:28 +0530

    Create Package

---
+++ company/Ch_65_Creating_Packages.java
@@ -0,0 +1,13 @@
+package com.company;
+//import java.util.Scanner;
+import java.util.*;
+
+public class Ch_65_Creating_Packages {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("This is Scanner Package object ");
+
+    }
+
+}
